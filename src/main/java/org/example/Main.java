eb1db63b52afe7413d138c54305a61b2eea2cb6e
Nebn1x <sashaproc2289@gmail.com>
2026-05-19 08:51:49 +0300

    Done Second Task from 18.05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,99 +1,97 @@
 package org.example;
 
+import org.example.entity.Circle;
 import org.example.entity.Figure;
 import org.example.randomthings.FigureSupplier;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         ///////////////////////////////
-        System.out.println("Part 1.1");
+        System.out.println("Part 2.1");
         FigureSupplier figureSupplier = new FigureSupplier();
         List<Figure> figureList = new ArrayList<>();
-        Map<String, List<Figure>> mapOfFigures = new HashMap<>();
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 15; i++) {
             figureList.add(figureSupplier.getRandomFigure());
         }
 
-        for (Figure figure : figureList) {
-            mapOfFigures.computeIfAbsent(figure.getClass().getSimpleName(), k -> new ArrayList<>()).add(figure);
-        }
+        Predicate<Figure> isLarge = f -> f.getArea() > 50;
+        Predicate<Figure> isRed = f -> f.getColor().equals("red");
 
-        for (Map.Entry<String, List<Figure>> entry : mapOfFigures.entrySet()) {
-            System.out.println(entry.getKey() + ":");
-            System.out.println("count = " + entry.getValue().size());
-            System.out.println("total area = " + String.format("%.2f",entry.getValue().stream().mapToDouble(Figure::getArea).sum()));
-        }
+        long countIsLarge = figureList.stream()
+                .filter(isLarge)
+                .count();
+        System.out.println("Large figures - " + countIsLarge);
+
+        long countIsLargeAndIsRed = figureList.stream()
+                .filter(isLarge.and(isRed))
+                        .count();
+        System.out.println("Large and Red figures - " + countIsLargeAndIsRed);
 
         ///////////////////////////////
-        System.out.println("\nPart 1.2");
-        Map<String, Set<Figure>> mapOfFiguresByColor = new HashMap<>();
-
-        for (Figure figure : figureList) {
-            mapOfFiguresByColor.computeIfAbsent(figure.getColor(), k -> new HashSet<>()).add(figure);
-        }
-        Set<Figure> testSet = new HashSet<>();
-        testSet.add(figureSupplier.getDefaultFigure());
-        testSet.add(figureSupplier.getDefaultFigure());
-        System.out.println("testSet " + testSet.size());
-        System.out.println("mapOfFiguresByColor " + mapOfFiguresByColor.size());
-
-        //////////////////////////////
-        System.out.println("\nPart 1.3");
-
-        Comparator<Figure> comparator = Comparator.comparingDouble(Figure::getArea);
-        List<Figure> sortedList = figureList.stream()
-                .sorted(comparator.reversed())
-                .limit(3)
+        System.out.println("Part 2.2");
+        List<String> result2_2 = figureList.stream()
+                .map(figure -> figure.getClass().getSimpleName()
+                        + " "
+                        + figure.getColor()
+                        + " area= "
+                        + String.format("%.2f",figure.getArea()))
                 .toList();
+        result2_2.forEach(System.out::println);
 
-        for (Figure figure : sortedList) {
-            System.out.println(figure.getClass().getSimpleName() + ": " + figure.getColor() + ", area:" + String.format(" %.2f",figure.getArea()));
-        }
+        ///////////////////////////////
+        System.out.println("Part 2.3");
+        Map<String, Long> result2_3 = figureList.stream()
+                .collect(Collectors.groupingBy(figure -> figure.getClass().getSimpleName(),
+                        Collectors.counting()
+                ));
+        result2_3.forEach((t, c) -> System.out.println("type- "+ t + ": " + "count = " + c));
 
-        //////////////////////////////
-        System.out.println("\nPart 1.4");
+        ///////////////////////////////
+        System.out.println("Part 2.4");
 
-        Map<String, Double> mapForArea = new HashMap<>();
-        Map<String, Integer> mapForCount = new HashMap<>();
+        Optional<Figure> firstCircle = figureList.stream()
+                .filter(figure -> figure instanceof Circle)
+                .findFirst();
 
-        for (Figure figure : figureList) {
-            mapForArea.merge(figure.getColor(), figure.getArea(), Double::sum);
-            mapForCount.merge(figure.getColor(), 1, Integer::sum);
-        }
+        String result = firstCircle
+                .map(figure ->
+                        figure.getClass().getSimpleName()
+                                + " "
+                                + figure.getColor()
+                                + " area = "
+                                + String.format("%.2f", figure.getArea()))
+                .orElse("no circle");
 
-        for (Map.Entry<String, Double> entry : mapForArea.entrySet()) {
-            System.out.println(entry.getKey() + " average - " + String.format("%.2f",entry.getValue()/mapForCount.get(entry.getKey())));
-        }
+        System.out.println(result);
 
-        //////////////////////////////
-        System.out.println("\nPart 1.5");
+        // список без Circle
 
-        Map<String, List<Figure>> unmodifiableMap =
-                Collections.unmodifiableMap(mapOfFigures);
-        try {
-            unmodifiableMap.put("Circle" , new ArrayList<>());
-        } catch (UnsupportedOperationException e) {
-            System.out.println("Unsupported operation");
-            //unmodifiableMap не дає міняти обгортку
-        }
+        List<Figure> withoutCircles = new ArrayList<>(figureList);
 
-        System.out.println("Before change :" + unmodifiableMap.get("Circle").size());
+        withoutCircles.removeIf(figure -> figure instanceof Circle);
 
-        mapOfFigures.get("Circle").add(figureSupplier.getRandomFigure());
+        Optional<Figure> noCircle = withoutCircles.stream()
+                .filter(figure -> figure instanceof Circle)
+                .findFirst();
 
-        System.out.println("After change :" + unmodifiableMap.get("Circle").size());
+        String resultWithoutCircle = noCircle
+                .map(figure ->
+                        figure.getClass().getSimpleName()
+                                +  figure.getColor()
+                                + " area= "
+                                + String.format("%.2f", figure.getArea()))
+                .orElse("no circle");
 
-        //все ще бачить зміни хоча не може сам поміняти
+        System.out.println(resultWithoutCircle);
     }
-
 }
